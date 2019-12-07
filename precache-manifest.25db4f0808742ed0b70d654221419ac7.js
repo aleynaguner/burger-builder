394dfdc440b9b140ccb0d99f3713b15eaef77cfb
Aleyna Güner <aleynaguner@hotmail.com>
2019-12-07 15:20:04 +0300

    Updates

---
+++ precache-manifest.25db4f0808742ed0b70d654221419ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c40e5615d13229f7c2c62edd6c44b17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f18c20fe32bb634d96",
+    "url": "/static/css/main.d4cfcd24.chunk.css"
+  },
+  {
+    "revision": "a4a2da64e41fcb3387a1",
+    "url": "/static/js/2.8d641d8c.chunk.js"
+  },
+  {
+    "revision": "a0f18c20fe32bb634d96",
+    "url": "/static/js/main.c54f6f3d.chunk.js"
+  },
+  {
+    "revision": "92ae4c9a8e2b0246cae1",
+    "url": "/static/js/runtime-main.38fae738.js"
+  }
+]);
